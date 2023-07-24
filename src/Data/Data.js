07d78142e00afd80be779d47260ca6ebe350e7e9
Javlon1 <muhamedmijotot@gmail.com>
2023-07-24 10:55:07 +0300

    qwe

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -161,13 +161,7 @@ export const portfolioData = [
                 name:"inware",
                 link:"https://inware.vercel.app/",
                 img:'img1'
-            },
-            {
-                id:2,
-                name:"test",
-                link:"https://inware.vercel.app/",
-                img:'img1'
-            },
+            }
         ]
     }
 ]
